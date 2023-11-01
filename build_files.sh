9f0bd817f8bfd76609e8cec60282de6d9a0dae41
Junaid Maqbool <lucifermjm@gmail.com>
2023-11-01 19:56:20 +0500

    vercel configuration write permisson

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
 apt-get update && apt-get install -y ffmpeg
+chmod 755 /app && sudo chmod 777 /app/downloads/output
 echo "BUILD END"
