9d5a6d54d69482008ae4bf22d9c64a9ca6cdc284
Junaid Maqbool <lucifermjm@gmail.com>
2023-11-15 15:19:05 +0500

    Adds stripe redirect

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 import datetime
+from datetime import timedelta
 import tempfile
 import math
+import stripe
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Query
+from supabase_py import create_client
 from starlette.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 from pydantic import BaseModel
 from .internals.utils import (
     generate_unique_id,
@@ -15,6 +20,15 @@ from .internals.utils import (
 )
 
 app = FastAPI()
+load_dotenv()
+
+# GET ENVS
+STRIPE_SECTET_KEY = os.getenv("STRIPE_SECTET_KEY")
+SUPA_BASE_URL = os.getenv("SUPABASE_PROJECT_URL")
+SUPA_BASE_KEY = os.getenv("SUPABASE_KEY")
+FRONTEND_URL = os.getenv('FRONTEND_URL')
+
+supabase_client = create_client(SUPA_BASE_URL, SUPA_BASE_KEY)
 
 origins = [
     "https://vid-slicer.vercel.app",
@@ -31,6 +45,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Item(BaseModel):
     url: str
     audio: bool
@@ -44,16 +59,58 @@ def status():
     return f"The API is working fine - {datetime.datetime.now()}".upper()
 
 
+@app.get("/order/success")
+async def order_success(session_id: str = Query(...,
+                                                title="Stripe Session",
+                                                description="Id of the Stripe session"
+                                                ), user_id: str = Query(...,
+                                                                        title="User ID",
+                                                                        description="Id of the user"
+                                                                        )):
+    try:
+        stripe.api_key = STRIPE_SECTET_KEY
+        session = stripe.checkout.Session.retrieve(session_id)
+        sub = stripe.Subscription.retrieve(session.subscription)
+
+        plan_id = sub['plan']['id']
+        plan_interval = sub['plan']['interval']
+        plan_interval_count = sub['plan']['interval_count']
+
+        if "year" in plan_interval:
+            days = 365 * plan_interval_count
+        elif "month" in plan_interval:
+            days = 30 * plan_interval_count
+        else:
+            days = 30
+
+        start_date = datetime.datetime.utcfromtimestamp(sub['start_date'])
+        expiry_date = start_date + timedelta(days=days)
+        data = {
+            "bought_at": start_date.strftime('%Y-%m-%d %H:%M:%S UTC'),
+            "expired_at": expiry_date.strftime('%Y-%m-%d %H:%M:%S UTC'),
+            "active": True,
+            "plan": sub['plan']['id'],
+            "stripe_customer_id": session['customer'],
+            "user_id": user_id
+        }
+        supabase_client.table("UserPlan").insert(data).execute()
+        return RedirectResponse(url="http://localhost:5173")
+        return JSONResponse(status_code=200, content={"data": data})
+    except Exception as e:
+        return JSONResponse(status_code=400, content={"error": str(e)})
+
+
 @app.get("/video_info")
 async def info_handler(url: str = Query(..., title="Item Name", description="Enter the item name")):
     try:
         status, options = get_video_information(url)
         if status:
-            return JSONResponse(status_code=200, content={ "data": options })
+            return JSONResponse(status_code=200, content={"data": options})
         else:
             raise Exception(options)
     except Exception as e:
         return JSONResponse(status_code=400, content={"error": str(e)})
+
 
 @app.post("/youtube-downloader/")
 async def youtube_downloader(item: Item):
@@ -82,4 +139,3 @@ async def youtube_downloader(item: Item):
 
     except Exception as e:
         return JSONResponse(status_code=400, content={"error": str(e)})
-
